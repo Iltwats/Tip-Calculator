66bfb7de3c87afa5119785f79cc811925eeb3943
Iltwats <2012atulsharma@gmail.com>
2022-01-02 23:45:21 +0530

    Instrumentation Tests for MainActivity

--- app/src/main/java/com/atul/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/atul/tipcalculator/MainActivity.kt
@@ -15,7 +15,7 @@ import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var billAmount: Int = 0
+    private var billAmount: Double = 0.0
     private var percentTip: Float = 0.0f
     private var totalBill: Double = 0.0
     private var timeNow: Long = 0L
@@ -30,40 +30,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTip() {
             // Bill Amount entered by the user
-            if (binding.textInputEditText.text.toString().isNotEmpty()) {
-                billAmount = binding.textInputEditText.text.toString().toIntOrNull()!!
-            }
+            billAmount = binding.textInputEditText.text.toString().toDoubleOrNull()!!
 
-            // Percentage tip calculator
-            percentTip = when (binding.radioGroup.checkedRadioButtonId) {
-                R.id.amazing -> 0.2F
-                R.id.good -> 0.15F
-                R.id.ok ->  0.1F
-                else -> 0.0F
-            }
+            if(binding.textInputEditText.text.toString().isNotEmpty()) {
+                // Percentage tip calculator
+                percentTip = when (binding.radioGroup.checkedRadioButtonId) {
+                    R.id.amazing -> 0.2F
+                    R.id.good -> 0.15F
+                    R.id.ok -> 0.1F
+                    else -> 0.01F
+                }
 
-            // Tip
-            var tip = billAmount*percentTip
+                // Tip
+                var tip = billAmount * percentTip
 
-            // Calculate total bill with tip percentage
-            totalBill = (billAmount + billAmount * percentTip).toDouble()
+                // Calculate total bill with tip percentage
+                totalBill = (billAmount + billAmount * percentTip).toDouble()
 
-            // Show only upto 4 decimal places after amount
-            totalBill = (totalBill * 10000.0).roundToLong() / 10000.0
+                // Show only upto 4 decimal places after amount
+                totalBill = (totalBill * 10000.0).roundToLong() / 10000.0
 
-            // Round off the number to nearest Integer
-            if (binding.switchButton.isChecked) {
-                totalBill = totalBill.roundToInt().toDouble()
-                tip = kotlin.math.ceil(tip)
-            }
+                // Round off the number to nearest Integer
+                if (binding.switchButton.isChecked) {
+                    totalBill = totalBill.roundToInt().toDouble()
+                    tip = kotlin.math.ceil(tip)
+                }
 
-            // Show the amount to user
-            /*
+                // Show the amount to user
+                /*
                 val local = ConfigurationCompat.getLocales(Resources.getSystem().configuration).get(0);
                 Toast.makeText(this,local.toString(),Toast.LENGTH_SHORT).show()
              */
-            val formattedTip = NumberFormat.getCurrencyInstance(Locale("en","in")).format(tip)
-            binding.finalAmount.text = "Tip Amount: $formattedTip"
+                val formattedTip = NumberFormat.getCurrencyInstance(Locale("en", "in")).format(tip)
+                binding.finalAmount.text = "Tip Amount: $formattedTip"
+            }
     }
 
     private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
